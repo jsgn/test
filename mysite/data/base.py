e0d9d8321b5a137493fe424b49ff1ab693072f36
sentieo <sentieo@shivs-MacBook-Air.local>
2015-04-07 04:41:14 +0530

    path

--- mysite/data/base.py
+++ mysite/data/base.py
@@ -2,10 +2,11 @@ __author__ = 'shivnarayan'
 import os
 import sys
 paths = ['/home/ubuntu/projects/','/home/ubuntu/projects/test/']
+# paths = ['/Users/sentieo/projects/test/','/Users/sentieo/projects/test/mysite/']
 for path in paths:
     if path not in sys.path:
         sys.path.append(path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from lxml.html import document_fromstring
 import re
 import urllib
